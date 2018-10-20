fb33b1bff0957e47061ff95568cb13a9184dc86a
Pedro Caleia <pmig.caleia@gmail.com>
2018-10-21 00:38:07 +0100

    Added validation to disallow an empty message

--- src/main/java/pt/pcaleia/util/test/exceptions/InvalidJsonException.java
+++ src/main/java/pt/pcaleia/util/test/exceptions/InvalidJsonException.java
@@ -1,6 +1,9 @@
 package pt.pcaleia.util.test.exceptions;
 
 
+import pt.pcaleia.util.ArgumentAssertions;
+
+
 /**
  * @author Pedro Caleia
  */
@@ -10,6 +13,8 @@ public final class InvalidJsonException extends RuntimeException {
 	
 	public InvalidJsonException( String message ) {
 		super( message );
+		
+		ArgumentAssertions.assertNotEmpty( message, "message" );
 	}
 	
 }
